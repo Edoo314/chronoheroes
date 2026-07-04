e29c3b7f41c548dcff4f5afc195e9017a8aa63d8
Edoo314 <edreuzy@proton.me>
2026-07-04 22:49:11 +0200

    fix: force-dynamic page personnage

--- app/[locale]/personnage/[slug]/page.tsx
+++ app/[locale]/personnage/[slug]/page.tsx
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
-
+export const dynamic = 'force-dynamic'
 export async function generateStaticParams() {
   const { data } = await supabase
     .from('persons')
